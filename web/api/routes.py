fe0c0cd76a111a20f2cf9342692522e96fad67e5
Geraldo Castro <geraldo.castro@thoughtworks.com>
2021-09-07 18:27:00 -0300

    Adiciona uma rota para listar os eventos de um diario e uma classe para capturar rotas validas no formato "yyyy-mm-dd"

--- web/api/routes.py
+++ web/api/routes.py
@@ -1,11 +1,26 @@
-from django.urls import include, path
+from datetime import datetime
+from django.urls import include, path, register_converter
 from rest_framework import routers
 from web.api.views import (
     CityCouncilAgendaView,
     CityCouncilAttendanceListView,
     GazetteView,
+    GazetteEventsView,
     HealthCheckView,
 )
+
+
+class DateConverter:
+    regex = '\d{4}-\d{2}-\d{2}'
+
+    def to_python(self, value):
+        return datetime.strptime(value, '%Y-%m-%d')
+
+    def to_url(self, value):
+        return value
+
+
+register_converter(DateConverter, 'yyyy')
 
 router = routers.DefaultRouter()
 router.register("", HealthCheckView, basename="root")
@@ -23,5 +38,10 @@ urlpatterns = [
         "city-council/attendance-list/",
         CityCouncilAttendanceListView.as_view(),
         name="city-council-attendance-list",
+    ),
+    path(
+        "gazette_events/<yyyy:date>",
+        GazetteEventsView.as_view(),
+        name="gazette-events"
     ),
 ]
